a20dbc80d4973434eb5a9ce100cb113a6a93ae5f
sedabull <sedabull@gmail.com>
2020-08-20 22:36:45 -0400

    complete assessment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
+
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
 import Clock from './components/clock/Clock';
 import Contact from './components/contact/Contact';
 import Welcome from './components/welcome/Welcome';
+import NotFound from './components/not-found/NotFound';
 import Navigation from './components/navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
         <Navigation />
-        <Route 
-            exact
-            path="/"
-            render={props => <Welcome {...props} name="Friend" />}
-        />
-        <Route path="/clock" component={Clock} />
-        <Route path="/contact" component={Contact} />
+        <Switch>
+            <Route 
+                exact
+                path="/"
+                render={props => <Welcome {...props} name="Friend"/>}
+            />
+            
+            <Route
+                exact
+                path="/welcome/:name"
+                render={props => <Welcome {...props} name={props.match.params.name}/>}
+            />
+            
+            <Route exact path="/clock" component={Clock} />
+            
+            <Route exact path="/contact" component={Contact} />
+            
+            <Route component={NotFound} />
+        </Switch>
     </div>
   );//end return JSX
 }//end App
---
+++ src/components/not-found/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NotFound(props) {
+    return (
+        <h1 className="NotFound">ERROR 404: Page Not Found</h1>
+    );//end return JSX
+}//end NotFound
+
+export default NotFound;
