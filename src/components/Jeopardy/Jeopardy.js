7a1bb67c46f6107b1f4cae3751c13f388fe7ebc6
sedabull <sedabull@gmail.com>
2020-08-28 12:17:53 -0400

    complete easy and medium requirements

--- src/components/Jeopardy/Jeopardy.js
+++ src/components/Jeopardy/Jeopardy.js
@@ -13,6 +13,7 @@ class Jeopardy extends Component {
         this.state = {
             score: 0,
             input: '',
+            hasAnswered: false,
             question: {
                 id: 0,
                 answer: '',
@@ -41,11 +42,54 @@ class Jeopardy extends Component {
         });//end getRandomQuestion
     }//end componentDidMount
 
+    isCloseEnough() {
+        let input = this.state.input || '';
+        let possibleAnswers = this.state.question.answer.match(/\w+/gi) || [];
+        
+        if(input.toLowerCase() === this.state.question.answer.toLowerCase()) return true;
+        for(let possibleAnswer of possibleAnswers) {
+            if(input.toLowerCase() === possibleAnswer.toLowerCase()) {
+                return true;
+            }//end if
+        }//end for
+
+        return false;
+    }//end isCloseEnough
+
     handleInput = event => {
         this.setState({ input: event.target.value })
     }//end handleInput
 
+    handleClick = event => {
+        this.setState(state => {
+            if(state.hasAnswered) {
+                this.client.getRandomQuestion().then(question => {
+                    this.setState({ question });
+                });//end getRandomQuestion
+                
+                return {
+                    input: '',
+                    hasAnswered: false
+                };//end return changes
+            } else {
+                let value = state.question.value;
+
+                return {
+                    hasAnswered: true,
+                    score: this.isCloseEnough() ? state.score + value : state.score - value
+                };//end return changes
+            }//end if/else
+        });//end setState
+    }//end handleClick
+
     render() {
+        let resultDiv = (
+            <div>
+                <h3>{this.isCloseEnough() ? 'Correct!' : 'Incorrect!'}</h3>
+                <p>{this.state.question.answer}</p>
+            </div>
+        );//end resultDiv
+
         return (
             <div className="Jeopardy">
                 <h1>Total Score: {this.state.score}</h1>
@@ -54,7 +98,9 @@ class Jeopardy extends Component {
                     value={this.state.question.value}
                     clue={this.state.question.question}
                 />
-                <input type="text" onChange={this.handleInput} value={this.input}/>
+                <input type="text" onChange={this.handleInput} value={this.input}/><br/>
+                <button onClick={this.handleClick}>{this.state.hasAnswered ? 'Next Question' : 'Check Answer'}</button>
+                {this.state.hasAnswered ? resultDiv : null}
             </div>
         );//end return JSX
     }//end render
